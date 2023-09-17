82e9d1307def48eea8885bbf9f60270191c712ce
CS50X-RGB <rohanchatterjee866@gmail.com>
2023-09-17 23:02:08 +0530

    Hero Section Home page

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -2,19 +2,47 @@ import { Link } from "react-router-dom";
 
 export default function Footer() {
   return (
-    <div className="flex flex-col space-y-7 font-chakra bg-black text-white items-center justify-center flex-end min-h-screen">
-      <div className="flex flex-col justify-center p-5">
-        <div className="flex flex-col text-center text-2xl">
-          <h1>I love Open sourcing if you found any bug lets fix it together</h1>
-          <Link to="https://github.com/CS50X-RGB/Chat.io" className="text-pink-300 hover:text-blue-500">
-            This Chat.io Frontend
-          </Link>
-          <Link to="https://github.com/CS50X-RGB/Chat.ioServer" className="text-pink-300 hover:text-blue-500">
-            This Chat.io Backend
-          </Link>
+    <>
+      <div className="flex flex-col space-y-7 font-chakra bg-black text-white items-center justify-center flex-end">
+        <div className="flex bg-blue-800/60 p-5 my-5 rounded-2xl  flex-row justify-center gap-4">
+          <div className="flex flex-col space-y-4">
+            <h1 className="text-4xl flex flex-col  justify-start text-left">
+              If you want to contact me
+            </h1>
+            <p className="text-lg">You can mail me here click on the button</p>
+          </div>
+          <div className="flex flex-row gap-5 items-center">
+            <a
+              href="mailto:rohanchatterjee866@gmail.com"
+              className="bg-blue-500 p-3 rounded-xl"
+            >
+              Send an Email
+            </a>
+          </div>
         </div>
-        <h1 className="flex justify-center">Made with love by Rohan Chatterjee ;)</h1>
+        <div className="flex flex-col justify-center p-5">
+          <div className="flex flex-col text-center text-2xl">
+            <h1>
+              I love Open sourcing if you found any bug lets fix it together
+            </h1>
+            <Link
+              to="https://github.com/CS50X-RGB/Chat.io"
+              className="text-pink-300 hover:text-blue-500"
+            >
+              This Chat.io Frontend
+            </Link>
+            <Link
+              to="https://github.com/CS50X-RGB/Chat.ioServer"
+              className="text-pink-300 hover:text-blue-500"
+            >
+              This Chat.io Backend
+            </Link>
+          </div>
+          <h1 className="flex justify-center">
+            Made with love by Rohan Chatterjee ;)
+          </h1>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,17 +1,68 @@
 import Header from "../Components/Header";
 import { useSelector } from "react-redux";
 import SideBar from "../Components/Sidebar";
-import Footer from '../Components/Footer';
+import Footer from "../Components/Footer";
+import image from "../assests/ChatAppHEro.webp";
+import { useEffect, useState } from "react";
+import { motion, useAnimation } from "framer-motion";
+
+const TypingAnimation = ({ text, speed }) => {
+  const [displayingText, setDisplayingText] = useState("");
+  const controls = useAnimation();
+
+  useEffect(() => {
+    const animateText = async () => {
+      for (let i = 0; i <= text.length; i++) {
+        await new Promise((resolve) => {
+          setTimeout(resolve, speed);
+        });
+        setDisplayingText(text.slice(0, i));
+      }
+    };
+    animateText().then(() => {
+      setDisplayingText(text);
+    });
+  }, [text, speed]);
+  console.log(displayingText);
+  return (
+    <motion.div
+      className="text-black"
+      initial={{ opacity: 1 }}
+      animate={controls}
+    >
+      {displayingText}
+    </motion.div>
+  );
+};
+
 export default function Home() {
   const isAuth = useSelector((store) => store.auth.isAuth);
   console.log(isAuth);
+
   return (
     <>
-    <div className="flex flex-col gap-4">
       <Header />
-      {isAuth ? <SideBar/> : null}
-    </div>
-    <Footer/>
+      <div className="flex flex-col gap-4">
+        {isAuth ? <SideBar /> : null}
+        <div className="flex flex-row justify-around p-8 bg-gradient-to-bl from-blue-300 via-pink-400 to-blue-500">
+          <div className="flex flex-col justify-center items-center">
+            <h1 className="text-blue-900 text-xl text-left md:text-5xl font-chakra font-bold flex flex-col items-center justify-center">
+              Welcome to RohanChat.io –
+              <TypingAnimation text="Connect Join Chat" speed={2000} />
+            </h1>
+            <p className="text-md font-chakra font-bold text-xl">
+              Chat.io is your go-to platform for connecting with people around
+              world and chatting in real-time.
+            </p>
+          </div>
+          <img
+            src={image}
+            className="w-1/4 h-1/4 rounded-xl rotate-12 p-6"
+            alt="ChatappHero"
+          />
+        </div>
+      </div>
+      <Footer />
     </>
   );
 }
